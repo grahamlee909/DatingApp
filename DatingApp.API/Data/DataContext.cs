1c53d28b9e2b74bcc696ef15c1fca23e6b5630d1
Graham Lee <grahamlee@Grahams-MacBook-Pro.local>
2020-08-06 14:11:52 +0100

    Auth repository added

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -3,13 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    public class DataContext : DbContext
-    {
-        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-        public DbSet<Value> Values { get; set; }   
-        
-        public DbSet<User> Users { get; set; }   
+    public DbSet<Value> Values { get; set; }
 
-    }
+    public DbSet<User> Users { get; set; }
+
+  }
 }
